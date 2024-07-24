21d2ae68c4cf9cfb8083eae1f48092a09e58cf8d
Ayanthan Easwaranathan <ayanthane@infyinnos.com>
2024-07-24 16:05:08 +0530

    DD

---
+++ aas.js
@@ -0,0 +1,18 @@
+console.log("test")
+var a=1
+var b=2
+var c=a+b
+console.log(c)
+function ayan()
+{
+    console.log(c)
+    let e=5
+    console.log(e)
+}
+
+ayan()
+ayan()
+console.log("hello world")
+console.log("*****************************************************")
+console.log(c)
+// console.log(e)
